d039b0ea179b08202c24df4d310372c6c6c1fb5e
Kim Do <kim.h.do@sjsu.edu>
2019-01-01 10:33:46 -0800

    update 1/1

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SolutionsComponent } from './solutions/solutions.component';
-import { CompanyComponent } from './company/company.component';
-import { ClientsComponent } from './clients/clients.component';
-import { CareersComponent } from './careers/careers.component';
-import { LoginComponent } from './login/login.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
+import { SolutionsComponent } from './components/solutions/solutions.component';
+import { CompanyComponent } from './components/company/company.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { LoginComponent } from './components/login/login.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { CareersComponent } from './components/careers/careers.component';
 
 const routes: Routes = [
     {path: "", component: HomeComponent},
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { SolutionsComponent } from './solutions/solutions.component';
-import { CompanyComponent } from './company/company.component';
-import { ClientsComponent } from './clients/clients.component';
-import { CareersComponent } from './careers/careers.component';
-import { LoginComponent } from './login/login.component';
-import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { SolutionsComponent } from './components/solutions/solutions.component';
+import { CompanyComponent } from './components/company/company.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { LoginComponent } from './components/login/login.component';
+import { ContactComponent } from './components/contact/contact.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { MaterialsModule } from './materials';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CareersComponent } from './components/careers/careers.component';
 
 @NgModule({
     declarations: [
