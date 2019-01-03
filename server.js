38d4973a518efcd30dba3755eade0938ebd2c5ee
Kim Do <kim.h.do@sjsu.edu>
2019-01-03 10:08:45 -0800

    navbar on top

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, './static')));
-app.use(express.static(  './frontend/dist/frontend' ));
+app.use(express.static('./frontend/dist/frontend'));
 
 
 app.set('views', path.join(__dirname, './client/views'));
@@ -16,9 +16,9 @@ app.set('view engine', 'ejs');
 // require("./server/config/mongoose.js")
 require("./server/config/routes.js")(app)
 
-app.all("*", (req,res,next) => {
+app.all("*", (req, res, next) => {
     res.sendFile(path.resolve("./frontend/dist/frontend/index.html"))
-  });
+});
 
 app.listen(8000, function () {
     console.log("listening on port 8000");
