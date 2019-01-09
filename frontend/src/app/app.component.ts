9632b7f03a833baf8c2b4cf331e3cd25384ddf59
Kim Do <kim.h.do@sjsu.edu>
2019-01-08 21:54:52 -0800

    transparent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SystemJsNgModuleLoader } from '@angular/core';
 import { Router } from '@angular/router';
 import * as $ from 'jquery';
 
@@ -13,6 +13,21 @@ export class AppComponent implements OnInit {
     constructor(private _router: Router) { }
 
     ngOnInit() {
+        $(document).ready(function () {
+            var lastScrollTop = 0;
+            $(window).scroll(function (event) {
+                var st = $(this).scrollTop();
+                if (st > lastScrollTop) {
+                    // downscroll code
+                    $( ".navbar" ).removeClass( "navbar-transparent bg-transparent" );
+                    $( ".navbar" ).addClass( "bg-dark navbar-transparent" );
+                } else if (st == 0) {
+                    $( ".navbar" ).addClass( "navbar-transparent bg-transparent" );
+                    $( ".navbar" ).removeClass( "bg-dark navbar-transparent" );
+                }
+                lastScrollTop = st;
+            });
+        });
     }
 
     onNavigate(text, flag) {
@@ -23,5 +38,7 @@ export class AppComponent implements OnInit {
         if (flag && window.innerWidth <= 900) {
             $('#hamburger').trigger('click');
         }
+
+        $("html, body").animate({ scrollTop: 0 }, "slow");
     }
 }
