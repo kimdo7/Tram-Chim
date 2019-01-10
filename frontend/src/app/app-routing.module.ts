ddf797718c98f0992360de82269eaac0a1ceb3e6
Kim Do <kim.h.do@sjsu.edu>
2019-01-09 21:50:10 -0800

    test

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { CareersComponent } from './components/careers/careers.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { ServicesComponent } from './components/services/services.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { PartnersComponent } from './components/partners/partners.component';
 
 const routes: Routes = [
-    {path: "", component: HomeComponent},
-    {path: 'services', component: ServicesComponent},
-    {path: 'about-us', component: AboutUsComponent},
-    {path: 'partners', component: PartnersComponent},
-    {path: 'careers', component: CareersComponent},
-    {path: 'contact-us', component: ContactComponent},
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,25 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { ContactComponent } from './components/contact/contact.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { MaterialsModule } from './materials';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CareersComponent } from './components/careers/careers.component';
-import { ServicesComponent } from './components/services/services.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { PartnersComponent } from './components/partners/partners.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent,
-        ServicesComponent,
-        AboutUsComponent,
-        PartnersComponent,
-        CareersComponent,
-        ContactComponent,
     ],
     imports: [
         BrowserModule,
